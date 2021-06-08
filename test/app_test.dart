fbd5b6a1e9c1e2f73f648bf4a021883b8ac62d8d
comrade77 <nimec77@gmail.com>
2021-06-08 13:45:13 +0700

    Пока закончил с тестами

--- test/app_test.dart
+++ test/app_test.dart
@@ -1,19 +1,71 @@
-// Copyright (c) 2021, Very Good Ventures
-// https://verygood.ventures
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
-import 'package:flutter_good/app/app.dart';
-import 'package:flutter_good/counter/counter.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_good/search/search.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:path_provider/path_provider.dart';
+
+import 'helpers/helpers.dart';
+
+class MockSearchBloc extends Mock implements SearchBloc {}
 
 void main() {
+  const length = 5;
+  late Widget app;
+  final searchResult = List.generate(length, (index) => TextData('Text $index', DateTime.now()));
+
+  final mockSearchBloc = MockSearchBloc();
+
+  setUpAll(() async {
+    HydratedBloc.storage = await HydratedStorage.build(
+      storageDirectory: await getApplicationDocumentsDirectory(),
+    );
+    whenListen(
+      mockSearchBloc,
+      Stream.fromIterable(<SearchState>[]),
+    );
+    when(mockSearchBloc.close).thenAnswer((_) => Future.value());
+    when(() => mockSearchBloc.state).thenAnswer((_) => SearchState.searchSuccess(searchResult));
+  });
+
+  setUp(() {
+    app = MultiBlocProvider(
+      providers: [
+        BlocProvider<SearchBloc>(
+          create: (context) => mockSearchBloc,
+        ),
+        BlocProvider<HistoryBloc>(
+          create: (context) => HistoryBloc(HistoryRepositoryImp()),
+        ),
+      ],
+      child: const SearchPage(),
+    );
+  });
+
   group('App', () {
-    testWidgets('renders CounterPage', (tester) async {
-      await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
-    }, tags: 'ignore');
+    testWidgets('render App', (tester) async {
+      await tester.pumpApp(app);
+      expect(find.byType(SearchPage), findsOneWidget);
+    });
+
+    testWidgets('render list on successful search', (tester) async {
+      await tester.pumpApp(app);
+      final listTilesFinder = find.byType(ListTile);
+      for (var i = 0; i < length; i++) {
+        final listTileFinder = listTilesFinder.at(i);
+        searchResult[i].maybeMap(
+          (textData) {
+            final listTileWidget = tester.firstWidget(listTileFinder) as ListTile;
+            final titleTextWidget = listTileWidget.title as Text;
+            expect(titleTextWidget.data, textData.text);
+            final subtitleTextWidget = listTileWidget.subtitle as Text;
+            expect(subtitleTextWidget.data, textData.createData.toString());
+          },
+          orElse: () => throw (AssertionError('Receive TextData error')),
+        );
+      }
+    });
   });
 }
